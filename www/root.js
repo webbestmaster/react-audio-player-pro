e980ff69d63ac8bc569fefe76586bb8948a6e6d0
dim <web.best.master@gmail.com>
2020-09-05 16:47:27 +0300

    remove extra code

--- www/root.js
+++ www/root.js
@@ -1,26 +1,3 @@
 // @flow
 
-/* global document, Event, HTMLTextAreaElement, HTMLDivElement, HTMLPreElement, HTMLInputElement */
-
-import {init} from './init';
-import defaultMarkdown from './demo.md';
-
-const input = document.querySelector('.js-input');
-const output = document.querySelector('.js-output');
-const outputDebug = document.querySelector('.js-output-debug');
-const useLineBreak = document.querySelector('.js-use-line-break');
-const parseLink = document.querySelector('.js-parse-link');
-
-if (
-    input instanceof HTMLTextAreaElement
-    && output instanceof HTMLDivElement
-    && outputDebug instanceof HTMLPreElement
-    && useLineBreak instanceof HTMLInputElement
-    && parseLink instanceof HTMLInputElement
-) {
-    input.textContent = defaultMarkdown;
-
-    init(input, output, outputDebug, useLineBreak, parseLink);
-
-    input.dispatchEvent(new Event('input'));
-}
+console.log('audio player pro');
