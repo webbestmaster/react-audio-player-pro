0c5ca263588a3365d29d74c11495900b8fbd66c5
dim <web.best.master@gmail.com>
2020-09-07 21:02:04 +0300

    remove extra code

--- src/lib/event.js
+++
@@ -1,5 +0,0 @@
-// @flow
-
-export function stopPropagation(evt: SyntheticEvent<HTMLElement>) {
-    evt.stopPropagation();
-}
--- src/lib/number.js
+++ src/lib/number.js
@@ -17,37 +17,3 @@ export function getRandom(fromInclude: number, toExclude: number, excludeList?: 
 
     return randomNumber;
 }
-
-export function addMetricPostfix(value: number): string {
-    if (value < 1e3) {
-        return String(value);
-    }
-
-    // 10^1		deca
-    // 10^2		hecto
-    // 10^3		kilo
-    // 10^6		mega
-    // 10^9		giga
-    // 10^12	tera
-    // 10^15	peta
-    // 10^18	exa
-    // 10^21	zetta
-    // 10^24	yotta
-    const postfixList = ['k', 'm', 'g', 't', 'p', 'e', 'z', 'y'];
-    const postfixListLength = postfixList.length;
-
-    // eslint-disable-next-line no-loops/no-loops
-    for (let postfixIndex = 0; postfixIndex < postfixListLength; postfixIndex += 1) {
-        const postfix = postfixList[postfixIndex];
-        const divider = Math.pow(10, (postfixIndex + 1) * 3);
-
-        if (value < divider * 1e3) {
-            const integer = Math.floor(value / divider);
-            const fractional = String(value % divider).padStart(3, '0');
-
-            return `${integer},${fractional[0]}${postfix}`;
-        }
-    }
-
-    return '999,9y';
-}
