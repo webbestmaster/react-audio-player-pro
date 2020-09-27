09cc9a3743a7235879fea66862035c401f80794c
dim <web.best.master@gmail.com>
2020-09-27 18:03:28 +0300

    WIP - context

--- src/audio-player/audio-player-head/audio-player-head-controls/c-audio-player-head-controls.js
+++ src/audio-player/audio-player-head/audio-player-head-controls/c-audio-player-head-controls.js
@@ -17,42 +17,66 @@ export class AudioPlayerHeadControls extends Component<PropsType, StateType> {
         this.state = {};
     }
 
+    renderButtonShuffle(): Node {
+        return (
+            <AudioPlayerControlButton
+                ariaLabel="shuffle"
+                imageId="button-shuffle"
+                isActive={false}
+                onClick={console.log}
+            />
+        );
+    }
+
+    renderButtonRepeat(): Node {
+        return (
+            <AudioPlayerControlButton
+                ariaLabel="repeat"
+                // imageId={repeatingState === repeatOne ? 'button-repeat-one' : 'button-repeat'}
+                imageId="button-repeat"
+                // isActive={[repeatOne, repeatAll].includes(repeatingState)}
+                isActive={false}
+                onClick={console.log}
+            />
+        );
+    }
+
+    renderButtonPrevTrack(): Node {
+        return <AudioPlayerControlButton ariaLabel="prev" imageId="button-prev-track" onClick={console.log}/>;
+    }
+
+    renderButtonPlay(): Node {
+        // {playingState !== playingStatePlaying
+        //     ? <AudioPlayerControlButton ariaLabel="play" imageId="button-play" onClick={handlePlay}/>
+        //     : <AudioPlayerControlButton ariaLabel="pause" imageId="button-pause" onClick={handlePause}/>}
+
+        return <AudioPlayerControlButton ariaLabel="pause" imageId="button-pause" onClick={console.log}/>;
+    }
+
+    renderButtonNextTrack(): Node {
+        return <AudioPlayerControlButton ariaLabel="next" imageId="button-next-track" onClick={console.log}/>;
+    }
+
+    renderButtonPlayList(): Node {
+        return (
+            <AudioPlayerControlButton
+                ariaLabel="list"
+                imageId="button-play-list"
+                isActive={false}
+                onClick={console.log}
+            />
+        );
+    }
+
     render(): Node {
         return (
             <div className={AudioPlayerHeadControlsStyle.audio_player_head_controls}>
-                <AudioPlayerControlButton
-                    ariaLabel="shuffle"
-                    imageId="button-shuffle"
-                    isActive={false}
-                    onClick={console.log}
-                />
-
-                <AudioPlayerControlButton
-                    ariaLabel="repeat"
-                    // imageId={repeatingState === repeatOne ? 'button-repeat-one' : 'button-repeat'}
-                    imageId="button-repeat"
-                    // isActive={[repeatOne, repeatAll].includes(repeatingState)}
-                    isActive={false}
-                    onClick={console.log}
-                />
-
-                <AudioPlayerControlButton ariaLabel="prev" imageId="button-prev-track" onClick={console.log}/>
-
-                {/*
-                {playingState !== playingStatePlaying
-                    ? <AudioPlayerControlButton ariaLabel="play" imageId="button-play" onClick={handlePlay}/>
-                    : <AudioPlayerControlButton ariaLabel="pause" imageId="button-pause" onClick={handlePause}/>}
-*/}
-
-                <AudioPlayerControlButton ariaLabel="pause" imageId="button-pause" onClick={console.log}/>
-
-                <AudioPlayerControlButton ariaLabel="next" imageId="button-next-track" onClick={console.log}/>
-                <AudioPlayerControlButton
-                    ariaLabel="list"
-                    imageId="button-play-list"
-                    isActive={false}
-                    onClick={console.log}
-                />
+                {this.renderButtonShuffle()}
+                {this.renderButtonRepeat()}
+                {this.renderButtonPrevTrack()}
+                {this.renderButtonPlay()}
+                {this.renderButtonNextTrack()}
+                {this.renderButtonPlayList()}
             </div>
         );
     }
--- src/audio-player/audio-player-head/audio-player-head-playing-bar/c-audio-player-head-playing-bar.js
+++ src/audio-player/audio-player-head/audio-player-head-playing-bar/c-audio-player-head-playing-bar.js
@@ -2,7 +2,12 @@
 
 import React, {Component, type Node} from 'react';
 
-import AudioPlayerHeadPlayingBarStyle from './audio-player-head-playing-bar.scss';
+import {Time} from '../../../layout/time/c-time';
+import {RangeBar} from '../../../layout/range-bar/c-range-bar';
+import {hasVolumeBar} from '../../../lib/system';
+import {AudioPlayerControlButton} from '../../../layout/audio-player-control-button/c-audio-player-control-button';
+
+import audioPlayerHeadPlayingBarStyle from './audio-player-head-playing-bar.scss';
 
 type PropsType = {};
 
@@ -15,7 +20,74 @@ export class AudioPlayerHeadPlayingBar extends Component<PropsType, StateType> {
         this.state = {};
     }
 
+    handleOnChangeProgressBar = () => {};
+
+    handleToggleMute = () => {};
+
+    handleOnChangeVolumeBar = (trackVolume: number) => {
+        // const audioTag = this.getAudioTag();
+        // if (!audioTag) {
+        //     return;
+        // }
+        // audioTag.volume = trackVolume;
+        // this.setState({trackVolume});
+    };
+
+    renderTime(): Node {
+        // const {state} = this;
+        // const {trackCurrentTime, trackFullTime} = state;
+
+        return <Time className={audioPlayerHeadPlayingBarStyle.time} currentTime={0} fullTime={1}/>;
+    }
+
+    renderProgressBar(): Node {
+        // const {state} = this;
+        // const {trackCurrentTime, trackFullTime} = state;
+
+        return <RangeBar onChange={this.handleOnChangeProgressBar} progress={10 / 20}/>;
+    }
+
+    renderSwitchSoundButton(): Node {
+        if (!hasVolumeBar) {
+            return null;
+        }
+
+        // const {state} = this;
+        // const {trackVolume, isMuted} = state;
+        // const isActualMuted = isMuted || trackVolume === 0;
+        // const soundImageSrc = isActualMuted ? 'button-sound-off' : 'button-sound-on';
+
+        const soundImageSrc = 1 ? 'button-sound-off' : 'button-sound-on';
+
+        return (
+            <AudioPlayerControlButton
+                ariaLabel="switch-sound"
+                className=""
+                imageId={soundImageSrc}
+                onClick={this.handleToggleMute}
+            />
+        );
+    }
+
+    renderVolumeBar(): Node {
+        if (!hasVolumeBar) {
+            return null;
+        }
+
+        // const {state} = this;
+        // const {trackVolume} = state;
+
+        return <RangeBar className="" onChange={this.handleOnChangeVolumeBar} progress={0.5}/>;
+    }
+
     render(): Node {
-        return <div className={AudioPlayerHeadPlayingBarStyle.audio_player_head_playing_bar}>playing_bar</div>;
+        return (
+            <div className={audioPlayerHeadPlayingBarStyle.audio_player_head_playing_bar}>
+                {this.renderTime()}
+                {this.renderProgressBar()}
+                {this.renderSwitchSoundButton()}
+                {this.renderVolumeBar()}
+            </div>
+        );
     }
 }
