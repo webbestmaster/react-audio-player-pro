95b8fbc2724ada2f9d16e86878344c456f2ee0b9
dim <web.best.master@gmail.com>
2020-10-05 21:06:58 +0300

    WIP - buttons

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,29 +1,52 @@
-/*
+/// <reference types="react" />
+
 declare module 'react-audio-player-pro' {
-/!*
-    export interface MarkdownConfigType {
-        useLineBreak?: boolean,
-        wrapperClassName?: string,
-        parseLink?: boolean,
-        codeHighlight?: (langName: string, code: string) => string,
-        useWrapper?: boolean,
+    export interface AudioPlayerListItemArtworkType {
+        src: string, // 'https://dummyimage.com/96x96',
+        sizes: string, // '96x96',
+        type: string, // 'image/png',
     }
 
-    export default function markdownPro(mdInput: string, config?: MarkdownConfigType): string;
+    export interface MediaMetadataType {
+        title: string,
+        artist?: string,
+        album?: string,
+        artwork?: Array<AudioPlayerListItemArtworkType>,
+    }
 
-    export function markdown(mdInput: string, config?: MarkdownConfigType): string;
-*!/
+    export interface TrackType {
+        src: string,
+        mediaMetadata?: MediaMetadataType,
+        // eslint-disable-next-line id-match
+        content?: React.ReactNode,
+    }
+
+    export const AudioPlayerControlSprite: (props: {}) => React.SFC<{}>;
+
+    export interface AudioPropsType {
+        src: string,
+        mediaMetadata?: MediaMetadataType,
+        className?: string,
+        onDidMount?: (audioNode: HTMLAudioElement | null) => void,
+        downloadFileName?: string,
+        useRepeatButton?: boolean,
+    }
+
+    export const Audio: (props: AudioPropsType) => React.SFC<AudioPropsType>;
+
+    export interface AudioPlayerPropsType {
+        trackList: Array<TrackType>,
+        className?: string,
+        onDidMount?: (audioNode: HTMLAudioElement | null) => void,
+    }
+
+    export const AudioPlayer: (props: AudioPlayerPropsType) => React.SFC<AudioPlayerPropsType>;
 }
-*/
 
-/*
 declare module 'react-audio-player-pro/dist/style.css' {
-/!*
     type StyleType = {};
 
     const style: StyleType;
 
     export default style;
-*!/
 }
-*/
