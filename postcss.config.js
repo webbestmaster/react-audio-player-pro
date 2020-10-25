03350995cd778c2e0b74225200847fa512c31f8a
dim <web.best.master@gmail.com>
2020-10-25 22:56:06 +0300

    remove extra file

--- postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require('autoprefixer');
-
-module.exports = ({file, options, environment}) => ({
-    plugins: [
-        autoprefixer({
-            overrideBrowserslist: ['last 1 version', '> 1%', 'IE 10'],
-        }),
-    ],
-});
