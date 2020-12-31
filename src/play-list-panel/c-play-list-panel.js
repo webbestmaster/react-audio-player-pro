bdb83eb7e15b52e26055adf2d2cf9607253a0de5
dim <web.best.master@gmail.com>
2020-12-31 17:51:29 +0300

    play-list providing - WIP

--- src/play-list-panel/c-play-list-panel.js
+++ src/play-list-panel/c-play-list-panel.js
@@ -3,8 +3,8 @@
 import React, {useContext, useState} from 'react';
 
 import {PlayListContext} from '../../www/js/provider/play-list/c-play-list-context';
-
 import {IsRender} from '../layout/is-render/c-is-render';
+import type {PlayListType} from '../../www/js/provider/play-list/play-list-context-type';
 
 import playListPanelStyle from './play-list-panel.scss';
 import type {PageNameType} from './play-list-panel-type';
@@ -15,8 +15,12 @@ type PropsType = {};
 
 export function PlayListPanel(props: PropsType): React$Node {
     const playListContextData = useContext(PlayListContext);
+    const allPlayLists = playListContextData.getAllPlayLists();
+
+    const defaultActivePlayList = allPlayLists.length > 0 ? allPlayLists[0] : null;
 
     const [pageName, setPageName] = useState<PageNameType>(playListPanelPageNameMap.listOfPlayList);
+    const [activePlayList, setActivePlayList] = useState<PlayListType | null>(defaultActivePlayList);
 
     console.log(playListContextData);
 
