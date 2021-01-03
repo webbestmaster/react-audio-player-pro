221ab09be12ece80a6ce767ecebe96846dc8bb5c
dim <web.best.master@gmail.com>
2021-01-03 18:51:59 +0300

    play-list providing - WIP

--- src/layout/drag-list/c-drag-list.js
+++ src/layout/drag-list/c-drag-list.js
@@ -1,8 +1,6 @@
 // @flow
 
-import React, {useState, useEffect} from 'react';
-
-import {getRandomString} from '../../lib/string';
+import React, {useState} from 'react';
 
 import dragListStyle from './drag-list.scss';
 
@@ -18,123 +16,25 @@ type PropsType = {|
 export function DragList(props: PropsType): React$Node {
     const {list, onChange} = props;
     const [isDragOver, setIsDragOver] = useState<boolean>(false);
-    const defaultIdList = getDragItemIdList(list);
+    const defaultIdList = ['before', ...getDragItemIdList(list), 'after'];
     const [dragItemIdList, setDragItemIdList] = useState<Array<string>>(defaultIdList);
-    const [isHiddenId, setIsHiddenId] = useState<string>('');
 
-    /*
-    function handleOnDragOver(evt: SyntheticEvent<DragEvent>) {
-        const newDragList = [...dragItemIdList];
+    const fullDragItemIdList = [...dragItemIdList].filter((id: string): boolean => {
+        return id !== 'before' && id !== 'after';
+    });
 
-        // setIsHiddenId('');
+    fullDragItemIdList.unshift('before');
 
-        if (!newDragList.includes(activeDragInfo.itemId)) {
-            // setDragItemIdList([activeDragInfo.itemId, ...newDragList]);
-            // return;
-        }
-
-        // const itemId = evt.dataTransfer.getData('drag-item-id');
-
-        console.log('list handleOnDragOver', dragItemIdList);
-
-        // evt.preventDefault();
-        // setIsDragOver(true);
-    }
-
-    function handleOnDragEnter(evt: SyntheticEvent<DragEvent>) {
-        const newDragList = [...dragItemIdList];
-
-        // setIsHiddenId('');
-
-        if (!newDragList.includes(activeDragInfo.itemId)) {
-            // setDragItemIdList([activeDragInfo.itemId, ...newDragList]);
-            // return;
-        }
-
-        console.log('list handleOnDragEnter', dragItemIdList.join('') + '/' + activeDragInfo.itemId);
-
-        evt.preventDefault();
-        setIsDragOver(true);
-    }
-
-    function handleOnDragLeave(evt: SyntheticEvent<DragEvent>) {
-        console.log('list handleOnDragLeave', dragItemIdList);
-
-        const idList = getDragItemIdList(list);
-
-        const itemId = activeDragInfo.itemId;
-
-        if (!idList.includes(itemId)) {
-            setDragItemIdList(idList);
-        }
-
-        setIsHiddenId(itemId);
-
-        setIsDragOver(false);
-    }
-
-    function handleOnDragEnd() {
-        console.log('list handleOnDragEnd', dragItemIdList);
-
-        onChange(dragItemIdList);
-        console.log(dragItemIdList);
-        console.log('LIST - handleOnDragEnd - END');
-    }
-
-    function handleOnDrop(evt: SyntheticEvent<DragEvent>) {
-        const itemId = activeDragInfo.itemId;
-
-        evt.preventDefault();
-
-        console.log('list handleOnDrop', dragItemIdList, itemId);
-
-        // setIsDragged(false);
-    }
-*/
-
-    /*
-        if (dragItemIdList.length === 0) {
-            const randomString = getRandomString();
-
-            return (
-                <ul
-                    data-is={isDragOver}
-                    key="empty"
-                    onDragEnd={handleOnDragEnd}
-                    onDragLeave={handleOnDragLeave}
-                    onDragOver={handleOnDragOver}
-                >
-                    <DragListItem
-                        dragList={dragItemIdList}
-                        isDragActive={isDragOver}
-                        item={{
-                            id: randomString + 1,
-                            node: 'no',
-                        }}
-                        key={randomString + 1}
-                        setDragList={setDragItemIdList}
-                    />
-                </ul>
-            );
-        }
-    */
+    fullDragItemIdList.push('after');
 
     return (
-        <div
-            className={dragListStyle.drag_list}
-            data-is={isDragOver}
-            // onDragEnd={handleOnDragEnd}
-            // onDragLeave={handleOnDragLeave}
-            // onDragEnter={handleOnDragEnter}
-            // onDragOver={handleOnDragOver}
-            // onDrop={handleOnDrop}
-        >
-            {dragItemIdList.map((dragItemId: string): React$Node => {
+        <div className={dragListStyle.drag_list} data-is={isDragOver}>
+            {fullDragItemIdList.map((dragItemId: string): React$Node => {
                 const dragListItem = getDragItemById(list, dragItemId);
 
                 const foreignItem: DragListItemType = {
                     id: dragItemId,
-                    node: 'no - node',
+                    node: dragItemId,
                 };
 
                 return (
--- src/layout/drag-list/drag-list-item/c-drag-list-item.js
+++ src/layout/drag-list/drag-list-item/c-drag-list-item.js
@@ -4,7 +4,6 @@ import React, {useState, useRef} from 'react';
 
 import type {DragListItemType} from '../drag-list-type';
 import {IsRender} from '../../is-render/c-is-render';
-
 import {activeDragInfo} from '../drag-list-helper';
 
 import dragListItemStyle from './drag-list-item.scss';
@@ -18,40 +17,22 @@ type PropsType = {|
 |};
 
 export function DragListItem(props: PropsType): React$Node {
-    const {item, setDragList, dragList, isDragActive} = props;
+    const {item, setDragList, dragList} = props;
     const {id, node} = item;
 
     const [isDragged, setIsDragged] = useState<boolean>(false);
-    const [isDragOverTop, setIsDragOverTop] = useState<boolean>(false);
-    const [isDragOverBottom, setIsDragOverBottom] = useState<boolean>(false);
 
     function handleOnDragStart() {
-        // console.log('item handleOnDragStart', id);
-
         activeDragInfo.itemId = id;
-
-        // evt.dataTransfer.setData('drag-item-id', id);
 
         setIsDragged(true);
     }
 
-    function handleOnDragEnd(evt) {
-        /*
-                if (id === activeDragInfo.itemId) {
-                    evt.preventDefault();
-                    evt.stopPropagation();
-                    setIsDragged(true);
-                    return;
-                }
-        */
-        console.log('item handleOnDragEnd', id);
+    function handleOnDragEnd() {
         setIsDragged(false);
     }
 
-    function handleOnDragOverTop(evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
-
+    function handleOnDragOver(shift: 1 | -1) {
         if (id === activeDragInfo.itemId) {
             return;
         }
@@ -62,137 +43,65 @@ export function DragListItem(props: PropsType): React$Node {
             const index = newDragList.indexOf(id);
 
             newDragList.splice(index, 0, activeDragInfo.itemId);
-            console.log('top');
 
             setDragList(newDragList);
             return;
         }
 
         const currentIndex = newDragList.indexOf(id);
-        const prevElement = newDragList[currentIndex - 1];
+        const siblingElement = newDragList[currentIndex + shift];
 
-        if (!prevElement) {
-            // setIsDragOverTop(true);
-            console.log('nononon');
+        if (!siblingElement) {
+            console.log('no sibling element');
             return;
         }
 
-        newDragList[currentIndex - 1] = id;
-        newDragList[currentIndex] = prevElement;
+        newDragList[currentIndex + shift] = id;
+        newDragList[currentIndex] = siblingElement;
 
         setDragList(newDragList);
     }
 
-    function handleOnDragLeaveTop(evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
+    function handleOnDragOverTop() {
+        handleOnDragOver(-1);
+    }
 
+    function handleOnDragOverBottom() {
+        handleOnDragOver(1);
+    }
+
+    function handleOnDragLeave() {
         if (id === activeDragInfo.itemId) {
             if (props.defaultIdList.includes(activeDragInfo.itemId)) {
                 return;
             }
             setDragList([...props.defaultIdList]);
         }
-
-        // setIsDragOverTop(false);
-    }
-
-    function handleOnDragOverBottom(evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
-
-        if (id === activeDragInfo.itemId) {
-            return;
-        }
-
-        console.log(id);
-        const newDragList = [...dragList];
-
-        const currentIndex = newDragList.indexOf(id);
-        const prevElement = newDragList[currentIndex + 1];
-
-        if (!newDragList.includes(activeDragInfo.itemId)) {
-            const index = newDragList.indexOf(id);
-
-            newDragList.splice(index, 0, activeDragInfo.itemId);
-
-            setDragList(newDragList);
-
-            return;
-        }
-
-        if (!prevElement) {
-            // setIsDragOverBottom(true);
-            console.log('nononon');
-            return;
-        }
-
-        newDragList[currentIndex + 1] = id;
-        newDragList[currentIndex] = prevElement;
-
-        setDragList(newDragList);
-    }
-
-    function handleOnDragLeaveBottom(evt) {
-        evt.preventDefault();
-        evt.stopPropagation();
-        console.log('my index', dragList.indexOf(id));
-
-        if (id === activeDragInfo.itemId) {
-            console.log('!!!!!!!');
-            if (props.defaultIdList.includes(activeDragInfo.itemId)) {
-                return;
-            }
-            setDragList([...props.defaultIdList]);
-        }
-
-        // setIsDragOverBottom(false);
     }
 
     return (
         <div
             className={dragListItemStyle.drag_list_item}
-            // style={{order: props.index}}
-            // onDragOver={handleOnDragOver}
-            // onDragLeave={handleOnDragLeave}
             data-id={id}
             draggable
             onDragEnd={handleOnDragEnd}
             onDragStart={handleOnDragStart}
         >
-            {/*
-            <IsRender isRender={isDragOverTop}>
-                <div>
-                    drag top
-                </div>
-            </IsRender>
-*/}
-
-            <IsRender isRender={isDragged === false}>
+            <IsRender isRender={isDragged === false && id !== activeDragInfo.itemId}>
                 <div
-                    // style={{pointerEvents: isDragActive ? 'initial' : 'none'}}
                     className={dragListItemStyle.drag_hunter_top}
-                    onDragLeave={handleOnDragLeaveTop}
+                    onDragLeave={handleOnDragLeave}
                     onDragOver={handleOnDragOverTop}
                 />
 
                 <div
-                    // style={{pointerEvents: isDragActive ? 'initial' : 'none'}}
                     className={dragListItemStyle.drag_hunter_bottom}
-                    onDragLeave={handleOnDragLeaveBottom}
+                    onDragLeave={handleOnDragLeave}
                     onDragOver={handleOnDragOverBottom}
                 />
             </IsRender>
 
             <div className={dragListItemStyle.content}>{node}</div>
-
-            {/*
-            <IsRender isRender={isDragOverBottom}>
-                <div>
-                    drag bottom
-                </div>
-            </IsRender>
-*/}
         </div>
     );
 }
