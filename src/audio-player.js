a39f3b0c9777073480a8ec5cefc2c081049c4dfc
dim <web.best.master@gmail.com>
2021-01-08 19:08:33 +0300

    update version

--- src/audio-player.js
+++ src/audio-player.js
@@ -3,6 +3,6 @@
 export {AudioPlayerControlSprite} from './layout/audio-player-control-sprite/c-audio-player-control-sprite';
 export {Audio} from './audio-player/audio/c-audio';
 export {AudioPlayer} from './audio-player/c-audio-player';
-export {PlayListContext} from './provider/play-list/play-list-context';
+// export {PlayListContext} from './provider/play-list/play-list-context';
 export {PlayListProvider} from './provider/play-list/c-play-list-context';
 export {PlayListPanel} from './play-list/play-list-panel/c-play-list-panel';
