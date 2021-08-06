9708efefc242ad01d385e3d2a99feeffc96ac910
dim <web.best.master@gmail.com>
2021-08-06 13:23:28 +0300

    update deps

--- webpack/setting/resolve/alias.js
+++ webpack/setting/resolve/alias.js
@@ -6,6 +6,7 @@ const {cwd} = require('../../config');
 
 const duplicateList = ['regenerator-runtime'];
 
+// eslint-disable-next-line unicorn/prefer-object-from-entries
 module.exports.alias = duplicateList.reduce((accumulator, packageName) => {
     return {...accumulator, [packageName]: path.resolve(cwd, `node_modules/${packageName}`)};
 }, {});
